a529d8411f3592137c5ff74491f2d5830363c81e
Jose <hose153@gmail.com>
2021-09-23 16:08:28 -0500

    default stuff

---
+++ src/reducers/countReducer.js
@@ -0,0 +1,13 @@
+import { INCREMENT } from '../actions'
+
+const counterReducer = (state = 0, action) => {
+	switch(action.type) {
+		case INCREMENT: 
+			return state + 1
+
+		default:
+			return state
+	}
+}
+
+export default counterReducer
