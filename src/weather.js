77b400d3d6f483e35a2b79de11bce677e2ced9ac
Jose <hose153@gmail.com>
2021-12-10 18:39:04 -0600

    Added D3 bar chart for week weather

--- src/weather.js
+++ src/weather.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import * as d3 from "d3";
 import { loadWeather } from './actions';
 import "./weather.css";
 import { useSelector, useDispatch } from 'react-redux'
@@ -6,7 +7,8 @@ import CurrentWeatherDisplay from './CurrentWeatherDisplay';
 import DailyWeatherDisplay from './DailyWeatherDisplay';
 import HourlyWeatherDisplay from './HourlyWeatherDisplay';
 import MinutelyWeatherDisplay from './MinetelyWeatherDisplay';
-
+import BarChart from './DailyWeatherBar';
+//import data from './weather.json'
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -14,13 +16,19 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function Weather() {
   const weather = useSelector(state => state.weather)
-  console.log("from weather")
-  console.log(weather)
+  //console.log("from weather")
+  //console.log(JSON.stringify(weather))
   const dispatch = useDispatch()
   const [lat, setLat] = useState('')
   const [long, setLong] = useState('')
+  // const [data, setData] = useState([])
   
-
+//   useEffect(() => {
+//     d3.json('./weather.json')
+//     .then(data => {
+//       setData(data)
+//     })
+// }, [])
     // async function assignVar() {
     //   const temp = weather.current.temp
     //   const wind_speed = weather.current.wind_speed
@@ -30,57 +38,105 @@ function Weather() {
     //     temp, wind_speed, feelsLike, humidity
   //   })
   // }
+  
+  // if (weather ===  null || {}) {
+  //   console.log(weather)
+  //   console.log('undefined')
+  //   return (
+  //     <div className="contianer-fluid">
+  //       <div className="weather-form col-4">
+  //         <form onSubmit={e => {
+  //           e.preventDefault();
+  //           dispatch(loadWeather(lat, long)
+  //           )
+  //         }}><heading className="form-title">Enter your coordinates here!</heading>
+  //           <div class="mb-3">
+  //             <label>Latitude:</label><br></br>
+  //             <input type="text" value={lat}
+  //               onChange={e => setLat(e.target.value)} />
+  //           </div>
+  //           <div class="mb-3">
+  //             <label>Longitude:</label><br></br>
+  //             <input type="text" value={long}
+  //               onChange={e => setLong(e.target.value)} />
+  //           </div>
+  //           <button>
+  //             Submit
+  //           </button>
+  //         </form>
+  //       </div>
 
-  return (
-    <div className="contianer-fluid">
-      <div className="weather-form col-4">
+  //       <div className=" container-fluid m-0 p-0">
+  //         <div className="page-one row pt-4 pb-4 justify-content-evenly">
+  //           {weather && <CurrentWeatherDisplay {...weather.current} />}
+  //           <div className="card-info  col-5">
+  //             <div className="row h-50 pb-3 justify-content-evenly">
+  //               <div className="day-bttn col-5" href="#Daily-weather">7 day Forecast</div>
+  //               <div className="other-bttn col-5"> 2 day hourly forecast</div>
+  //             </div>
+  //             <div className="row h-50 pt-3 justify-content-evenly">
+  //               <div className="other-bttn col-5"> Minutely forecast</div>
+  //               <div className="day-bttn col-5"> Previous 5 days</div>
+  //             </div>
+  //           </div>
+  //         </div>
+  //       </div>
 
-        <form onSubmit={e => {
-          e.preventDefault();
-          dispatch(loadWeather(lat, long))
+  //     </div>
+  //   );
 
-        }}><heading className="form-title">Enter your coordinates here!</heading>
-          <div class="mb-3">
-            <label>Latitude:</label><br></br>
-            <input type="text" value={lat}
-              onChange={e => setLat(e.target.value)} />
-          </div>
-          <div class="mb-3">
-            <label>Longitude:</label><br></br>
-            <input type="text" value={long}
-              onChange={e => setLong(e.target.value)} />
-          </div>
-          <button>
-            Submit
-          </button>
-        </form>
-      </div>
-
-      <div className=" container-fluid m-0 p-0">
-        <div className="page-one row pt-4 pb-4 justify-content-evenly">
-          
-            {weather && <CurrentWeatherDisplay {...weather.current} />}
-          
-          
-          <div className="card-info  col-5">
-            <div className="row h-50 pb-3 justify-content-evenly">
-              
-              <div className="day-bttn col-5" href="#Daily-weather">7 day Forecast</div>
-              <div className="other-bttn col-5"> 2 day hourly forecast</div>
+  // }
+  // else {
+    console.log(weather)
+    console.log('Display')
+    return (
+      <div className="contianer-fluid">
+        <div className="weather-form col-4">
+          <form onSubmit={e => {
+            e.preventDefault();
+            dispatch(loadWeather(lat, long)
+            )
+          }}><heading className="form-title">Enter your coordinates here!</heading>
+            <div class="mb-3">
+              <label>Latitude:</label><br></br>
+              <input type="text" value={lat}
+                onChange={e => setLat(e.target.value)} />
             </div>
-            <div className="row h-50 pt-3 justify-content-evenly">
-              <div className="other-bttn col-5"> Minutely forecast</div>
-              <div className="day-bttn col-5"> Previous 5 days</div>
+            <div class="mb-3">
+              <label>Longitude:</label><br></br>
+              <input type="text" value={long}
+                onChange={e => setLong(e.target.value)} />
+            </div>
+            <button>
+              Submit
+            </button>
+          </form>
+        </div>
+
+        <div className=" container-fluid m-0 p-0">
+          <div className="page-one row pt-4 pb-4 justify-content-evenly">
+            {weather && <CurrentWeatherDisplay {...weather.current} />}
+            <div className="card-info  col-5">
+              <div className="row h-50 pb-3 justify-content-evenly">
+                <div className="day-bttn col-5" href="#Daily-weather">7 day Forecast</div>
+                <div className="other-bttn col-5"> 2 day hourly forecast</div>
+              </div>
+              <div className="row h-50 pt-3 justify-content-evenly">
+                <div className="other-bttn col-5"> Minutely forecast</div>
+                <div className="day-bttn col-5"> Previous 5 days</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      
-      {weather && <DailyWeatherDisplay {...weather.daily} />} 
-      {weather && <HourlyWeatherDisplay {...weather.hourly} />}
-      {weather && <MinutelyWeatherDisplay {...weather.minutely} />}
-    </div>
-  );
-}
 
-export default Weather
+        {weather.daily && <DailyWeatherDisplay {...weather.daily} />}
+        {weather.daily && <BarChart {...weather} />}
+        {/* {loadData} */}
+        {/* <BarChart data = {data} /> */}
+        {weather.daily && <HourlyWeatherDisplay {...weather.hourly} />}
+        {weather.daily && <MinutelyWeatherDisplay {...weather.minutely} />}
+      </div>
+    );
+  }
+//}
+  export default Weather
